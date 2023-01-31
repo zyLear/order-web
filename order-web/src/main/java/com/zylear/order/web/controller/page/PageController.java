0c195f3729777f546cfeda001b784a4c8f85f4e8
zylear <284449710@qq.com>
2023-02-01 00:38:20 +0800

    thymeleaf learn

--- order-web/src/main/java/com/zylear/order/web/controller/page/PageController.java
+++ order-web/src/main/java/com/zylear/order/web/controller/page/PageController.java
@@ -1,4 +1,4 @@
-package com.zylear.order.web.controller.api;
+package com.zylear.order.web.controller.page;
 
 import com.zylear.order.web.dao.OrderInfoDao;
 import com.zylear.order.web.model.OrderInfoEntity;
@@ -7,19 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
-@RequestMapping("/api/order")
-public class OrderController {
+@RequestMapping("/page/order")
+public class PageController {
 
     @Autowired
     private OrderInfoDao orderInfoDao;
 
     @GetMapping("/list")
-    @ResponseBody
-    public Iterable<OrderInfoEntity> hello() {
-        Iterable<OrderInfoEntity> all = orderInfoDao.findAll();
-        return all;
+    public ModelAndView hello() {
+        Optional<OrderInfoEntity> byId = orderInfoDao.findById(1L);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("orderInfo", byId.get());
+        modelAndView.setViewName("ok");
+        return modelAndView;
     }
 
 
