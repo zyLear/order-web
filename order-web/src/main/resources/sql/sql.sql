a58bf6d984c33593e087e28aa3ad522e10ccfde6
zylear <284449710@qq.com>
2023-02-05 00:45:26 +0800

    sql

--- order-web/src/main/resources/sql/sql.sql
+++ order-web/src/main/resources/sql/sql.sql
@@ -5,13 +5,13 @@ USE `app_order`;
 
 CREATE TABLE `order_info`
 (
-    `id`               BIGINT(19,0) NOT NULL AUTO_INCREMENT,
-    `phone_number`     VARCHAR(32)    NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
-    `order_status`     TINYINT(3,0) NOT NULL,
-    `remark`           VARCHAR(255)   NOT NULL COLLATE 'utf8mb4_0900_ai_ci',
+    `id`               BIGINT(20) NOT NULL AUTO_INCREMENT,
+    `phone_number`     VARCHAR(32)    NOT NULL COLLATE 'utf8mb4_general_ci',
+    `order_status`     TINYINT(4) NOT NULL,
+    `remark`           VARCHAR(255)   NOT NULL COLLATE 'utf8mb4_general_ci',
     `price`            DECIMAL(10, 2) NOT NULL DEFAULT '0.00',
     `finish_time`      DATETIME NULL DEFAULT NULL,
-    `create_time`      DATETIME       NOT NULL,
-    `last_update_time` TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `create_time`      DATETIME       NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_update_time` TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`) USING BTREE
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4_general_ci;
